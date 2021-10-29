f6bb2997493274f3eed7048b0d3aa765e9ee05d7
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2021-10-29 20:57:35 +1030

    Add world map and bunny atlas

---
+++ TileSet/Cosy Farm Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Cosy Farm Tileset" tilewidth="16" tileheight="16" tilecount="1584" columns="44">
+ <image source="C:/Users/jeann/Downloads/farm full/farm full/Tileset/all tiles.png" width="704" height="576"/>
+</tileset>
